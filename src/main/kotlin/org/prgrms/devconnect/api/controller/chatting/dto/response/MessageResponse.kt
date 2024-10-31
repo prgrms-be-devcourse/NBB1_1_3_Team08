1b33f4a1e81eb8eed7dc691418e158d951838ef1
ECON <lgyoon621@naver.com>
2024-10-31 11:18:15 +0900

    fix: MessageResponse null 허용

--- src/main/kotlin/org/prgrms/devconnect/api/controller/chatting/dto/response/MessageResponse.kt
+++ src/main/kotlin/org/prgrms/devconnect/api/controller/chatting/dto/response/MessageResponse.kt
@@ -8,17 +8,17 @@ import java.time.LocalDateTime
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class MessageResponse(
     @Schema(description = "메시지 ID", example = "10")
-    val messageId: Long,
+    val messageId: Long?,
 
     @Schema(description = "보낸 사람의 ID", example = "2")
-    val senderId: Long,
+    val senderId: Long?,
 
     @Schema(description = "보낸 사람의 닉네임", example = "김철수")
     val nickname: String,
 
     @Schema(description = "메시지 내용", example = "안녕하세요!")
-    val content: String,
+    val content: String?,
 
     @Schema(description = "메시지 생성 시간", type = "string", format = "date-time", example = "2024-10-01T12:34:56")
-    val createdAt: LocalDateTime
+    val createdAt: LocalDateTime?
 )
