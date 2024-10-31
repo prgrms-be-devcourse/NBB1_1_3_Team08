377e0654a8b91a37f5016c71c0b28eafd17336df
ECON <lgyoon621@naver.com>
2024-10-31 11:18:41 +0900

    feat: ChattingService Java to Kotlin Migration

---
+++ src/main/kotlin/org/prgrms/devconnect/api/service/chatting/query/ChattingQueryService.kt
@@ -0,0 +1,37 @@
+package org.prgrms.devconnect.api.service.chatting.query
+
+import org.prgrms.devconnect.api.controller.chatting.dto.response.ChatRoomListResponse
+import org.prgrms.devconnect.api.controller.chatting.dto.response.MessageFullResponse
+import org.prgrms.devconnect.common.exception.ExceptionCode
+import org.prgrms.devconnect.common.exception.chatting.ChattingException
+import org.prgrms.devconnect.domain.chatting.entity.ChattingRoom
+import org.prgrms.devconnect.domain.chatting.repository.ChatParticipationRepository
+import org.prgrms.devconnect.domain.chatting.repository.ChattingRoomRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class ChattingQueryService(
+    private val chatParticipationRepository: ChatParticipationRepository,
+    private val chattingRoomRepository: ChattingRoomRepository,
+) {
+    fun findAllActivateChattingsByMemberId(memberId: Long): List<ChatRoomListResponse>? {
+        return chatParticipationRepository.findByMemberIdAndStatusIsActive(memberId)
+    }
+
+    // 채팅방 메세지 조회하는 메서드
+    fun getAllMessageByRoomId(roomId: Long, pageable: Pageable): MessageFullResponse {
+        val content = chatParticipationRepository.findAllMessageByRoomId(roomId, pageable)
+        return MessageFullResponse(
+            roomId = roomId,
+            messageList = content)
+    }
+
+    // 채팅방 ID로 채팅방 엔티티를 가져오는 메서드
+    fun getChatRoomById(chatroomId: Long): ChattingRoom {
+        return chattingRoomRepository.findById(chatroomId)
+            .orElseThrow { ChattingException(ExceptionCode.NOT_FOUND_CHATROOM) }
+    }
+}
