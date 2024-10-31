6d2fc8733a83d2743466deae686b78ebeef5244f
j-ra1n <j_ra1n@naver.com>
2024-10-31 16:56:28 +0900

    feat 사람인 api Runner작성

---
+++ src/main/kotlin/org/prgrms/devconnect/external/saramin/JobPostRunner.kt
@@ -0,0 +1,18 @@
+package org.prgrms.devconnect.external.saramin
+
+import org.prgrms.devconnect.api.service.jobpost.JobPostCommandService
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class JobPostRunner(
+        private val jobPostCommandService: JobPostCommandService
+) : ApplicationRunner {
+
+    // 애플리케이션 실행 시 호출되는 메서드
+    @Throws(Exception::class)
+    override fun run(args: ApplicationArguments?) {
+         // jobPostCommandService.saveJobPosts()
+    }
+}
