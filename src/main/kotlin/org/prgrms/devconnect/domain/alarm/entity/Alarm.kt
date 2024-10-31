d33bd8de87b9bd30d6007f9819f08e748ccc5c62
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-31 15:19:11 +0900

    feat: Alarm 엔티티 작성

--- src/main/kotlin/org/prgrms/devconnect/domain/alarm/entity/Alarm.kt
+++ src/main/kotlin/org/prgrms/devconnect/domain/alarm/entity/Alarm.kt
@@ -1,6 +1,7 @@
 package org.prgrms.devconnect.domain.alarm.entity
 
 import jakarta.persistence.*
+import org.prgrms.devconnect.domain.CreateTimestamp
 import org.prgrms.devconnect.domain.Timestamp
 import org.prgrms.devconnect.domain.member.entity.Member
 
@@ -10,21 +11,21 @@ class Alarm(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "alarm_id")
-    private var alarmId: Long? = null,
+    var alarmId: Long? = null,
 
     @Column(name = "alert_text", length = 500)
-    private var alertText: String,
+    var alertText: String,
 
     @Column(name = "related_url", length = 500)
-    private var relatedUrl: String,
+    var relatedUrl: String,
 
     @Column(name = "is_read")
-    private var isRead: Boolean = false,
+    var isRead: Boolean = false,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-    private val member: Member
-) : Timestamp() {
+    val member: Member
+) : CreateTimestamp() {
     fun updateAlarmStatusToRead() {
         this.isRead = true
     }
